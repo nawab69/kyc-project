4dfb0aed6dc555e21cf75614b318be26d4bf9dbd
nawab <nawabkhairuzzaman@gmail.com>
2021-08-12 22:31:12 +0600

    fix mongodb url

--- connectDB.js
+++ connectDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  let connection = process.env.DB;
   try {
-    const con = await mongoose.connect("mongodb://127.0.0.1:27017/testdb", {
+    const con = await mongoose.connect(`${connection}`, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
